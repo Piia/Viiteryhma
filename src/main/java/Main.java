e4f0b231fccc4ecc6b7a1770e2414ba02a771b13
Piia <piia.hartikka@helsinki.fi>
2017-04-02 20:50:26 +0300

    hello world

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Anna viite: ");
+        String viite = scanner.nextLine();
+
+        System.out.println("Annoit viitteen: " + viite);
+    }
+}
