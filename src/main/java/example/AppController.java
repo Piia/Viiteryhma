9f5ca7312205f957eddf593ef7802f627ae716d9
jannejau <janne.jau@cs.helsinki.fi>
2017-04-07 00:18:28 +0300

    BibTex export from mock db, AppController tests

--- src/main/java/example/AppController.java
+++ src/main/java/example/AppController.java
@@ -1,11 +1,19 @@
 package example;
 
 import example.References.Article;
+import example.bibTex.References;
+import example.bibTex.referencesToBibTex;
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AppController {
@@ -39,10 +47,41 @@ public class AppController {
     }
     
     @PostMapping("/export")
-    public String postExportReferences(Model model) {
-        // TODO: Create using BibTex
-        model.addAttribute("filePath", "/linkki.html");
+    @ResponseBody
+    public FileSystemResource postExportReferences(Model model) {
+        File generated = this.generateBibTex();
+        
+        model.addAttribute("filePath", "/files/references.bib");
         model.addAttribute("success", true);
-        return "export";
+        
+        return new FileSystemResource(generated);
+    }
+    
+    public File generateBibTex() {
+        try {
+            
+            List<Article> references = MockDatabase.getAll();
+            File l = new File("src/main/resources/static/files/references.bib");
+            referencesToBibTex k = new References(l);
+
+            for (Article ref : references) {
+                k.HederAndItsType(ref.getKey(), ref.getType());
+                Map<String,String> fields = ref.getFields();
+
+                fields.keySet().stream().forEach((key) -> {
+                    k.OneFieldAndItsType(fields.get(key), key);
+                });
+
+                k.EndReference();
+            }
+
+            k.EndFile();
+                        
+            return l;
+        } catch (Exception e) {
+            System.out.println("Error generating BibTeX: " + e);
+            return null;
+        }
+        
     }
 }
--- src/main/java/example/MockDatabase.java
+++ src/main/java/example/MockDatabase.java
@@ -23,4 +23,8 @@ public class MockDatabase {
     public static List<Article> getAll() {
         return references;
     }
+    
+    public static void reset() {
+        references.clear();
+    }
 }
