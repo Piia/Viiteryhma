ee92a7d6189c78652b7f9df9b661801f83040853
Hirvonen Tiera <tierahir@da5-cs-b221-05.pc.helsinki.fi>
2017-04-07 10:32:46 +0300

    cumberit (puolivalmiit)

---
+++ src/test/java/example/Stepdefs.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import example.bibTex.References;
+import java.io.File;
+import java.util.Scanner;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tierahir
+ */
+public class Stepdefs {
+
+    References r;
+    File l;
+
+    @Given("^References is initialized$")
+    public void references_is_initialized() throws Throwable {
+        l = new File("test3.bib");
+        r = new References(l);
+    }
+
+    @When("^heder_and_its_type_is_set (\\d+)(+\\d+)$")
+    public void heder_and_its_type_is_set(String heder, String type) throws Throwable {
+        r.HederAndItsType(heder, type);
+    }
+
+    @When("^reference_list_is_closed$")
+    public void reference_list_is_closed() throws Throwable {
+        r.EndReference();
+        r.EndFile();
+    }
+    
+    @Then("^the value should be (\\d+)$")
+    public void the_value_should_be(String val) throws Throwable {
+        Scanner t = new Scanner(l);
+        String g = t.nextLine();
+        assertEquals(val, g);
+    }
+}
