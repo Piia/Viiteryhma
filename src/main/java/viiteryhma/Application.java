482c401121fb9e8f409983a55fa9c704a0ac1287
teiran <tiera.hirvonen@gmail.com>
2017-04-19 22:26:11 +0300

    rivikattavuus tarkistettu

--- src/main/java/viiteryhma/Application.java
+++ src/main/java/viiteryhma/Application.java
@@ -1,7 +1,6 @@
 package viiteryhma;
 
 import example.bibTex.ReferenceToString;
-import example.bibTex.References;
 import example.bibTex.referencesToBibTex;
 import java.io.File;
 import java.io.IOException;
